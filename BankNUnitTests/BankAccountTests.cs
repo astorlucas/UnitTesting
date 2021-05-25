e544fa99effc98640973d5e1f24917a0872995eb
astorlucas <lucasastor@gmail.com>
2021-05-25 17:07:05 -0300

    commit final

--- BankNUnitTests/BankAccountTests.cs
+++ BankNUnitTests/BankAccountTests.cs
@@ -2,79 +2,72 @@ using Bank;
 using System;
 using NUnit.Framework;
 
+
+//Cambios para entrega
+
 namespace BankNUnitTests
 {
     public class BankAccountTests
     {
         private BankAccount account;
+        private BankAccount otherAccount;
+        private BankAccount noneAccount;
 
         [SetUp]
         public void Setup()
         {
-            // ARRANGE
+            //Abro una nueva cuenta con fondos
             account = new BankAccount(1000);
+            //Abro una segunda cuenta con fondos
+            otherAccount = new BankAccount(1000);
+            //Cuenta null
+            noneAccount = null;
         }
-
+        //1
         [Test]
-        public void Adding_Funds_Updates_Balance()
+        public void Test_Retiro_Fondos()
         {
-            // ACT
-            account.Add(500);
-
-            // ASSERT
-            Assert.AreEqual(1500, account.Balance);
-        }
-
-        [Test]
-        public void Adding_Negative_Funds_Throws()
-        {
-            // ACT + ASSERT
-            Assert.Throws<ArgumentOutOfRangeException>(() => account.Add(-500));
-        }
-
-        [Test]
-        public void Withdrawing_Funds_Updates_Balance()
-        {
-            // ACT
+            //Retiro fonsos
             account.Withdraw(500);
-
-            // ASSERT
+            //Controlo que se el balance es el correcto
             Assert.AreEqual(500, account.Balance);
         }
-
+        //2
         [Test]
-        public void Withdrawing_Negative_Funds_Throws()
+        public void Test_Agrego_Fondos()
         {
-            // ACT + ASSERT
+            //Agrego fondos
+            account.Add(600);
+            //Controlo que el balance es el correcto
+            Assert.AreEqual(1600, account.Balance);
+        }
+        //3
+        [Test]
+        public void Test_Transferir_A_Otros()
+        {
+            //Transfiero a otra cuenta
+            account.TransferFundsTo(otherAccount,100);
+
+            //Controlo disminución en acc 1 y aumento en acc 2
+            Assert.AreEqual(900,account.Balance);
+            Assert.AreEqual(1100,otherAccount.Balance);
+        }
+        //4
+        [Test]
+        public void Test_Retiro_Negativo()
+        {
+            //Retiro de un número negativo
+            //Como el error se ejecuta mediante un throw, realizo un assert conteniendo la función Withdraw
             Assert.Throws<ArgumentOutOfRangeException>(() => account.Withdraw(-500));
         }
 
+        //5
         [Test]
-        public void Withdrawing_More_Than_Balance_Throws()
+        public void Test_Trn_Cuenta_Inexistente()
         {
-            // ACT + ASSERT
-            Assert.Throws<ArgumentOutOfRangeException>(() => account.Withdraw(2000));
-        }
-
-        [Test]
-        public void Transfering_Funds_Updates_Both_Accounts()
-        {
-            // ARRANGE
-            var otherAccount = new BankAccount();
-
-            // ACT
-            account.TransferFundsTo(otherAccount, 500);
-
-            // ASSERT
-            Assert.AreEqual(500, account.Balance);
-            Assert.AreEqual(500, otherAccount.Balance);
-        }
-
-        [Test]
-        public void Transfer_To_Non_Existing_Account_Throws()
-        {
-            // ACT + ASSERT
-            Assert.Throws<ArgumentNullException>(() => account.TransferFundsTo(null, 2000));
+            //Transfiero a una cuenta declarada como null
+            //Como el error se ejecuta metiante un throw, realizo un asser contendiendo a la funcion TransferFundsTo
+            Assert.Throws<ArgumentNullException>(() => account.TransferFundsTo(noneAccount, 100));
         }
     }
 }
